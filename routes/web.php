4ffb5134aa48ca3bfa5f42a8aad5a6d2587c0c04
Ridwan Maulana <rmfhdofficial@gmail.com>
2026-05-11 05:24:03 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,9 @@ Route::get('/simulasi-sorting', function () {
     return view('simulasi-sorting');
 })->name('sorting.simulation');
 
+Route::get('/simulasi-sorting-person', function () {
+    return view('simulasi-sorting-person');
+})->name('sorting.simulation.person');
+
 Route::post('/simpan-hasil', [LKPDController::class, 'store'])->name('sorting.store');
 Route::get('/lkpd-stats', [LKPDController::class, 'getStats'])->name('sorting.stats');
